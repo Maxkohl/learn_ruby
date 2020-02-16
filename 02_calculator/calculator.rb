2549d9f1e998522e5345cc1f63d429ef233b54c5
Max K <maxdkohl@gmail.com>
2020-02-15 22:14:20 -0500

    Finished most calculator exercises

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,27 @@
-#write your code here
+def add (num1, num2)
+    (num1 + num2)
+
+end
+
+def subtract (num1, num2)
+    (num1 - num2)
+
+end
+
+def sum (array)
+    result = 0
+    i = 0
+    if array.length == 0
+        0
+    elsif
+        while i < array.length do
+            result += array[i]
+            i += 1
+        end
+    end
+    result
+end
+
+def multiply (num1, num2)
+    num1 * num2
+end
