93696e69c38a932b5345e2e3b737ddf71c3607ce
Max K <maxdkohl@gmail.com>
2020-02-15 23:11:57 -0500

    Began working on 05 pig latin exercise

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,19 @@
-#write your code here
+
+def translate(word)
+    vowel_array = ["a","e","i","o","u"]
+    if vowel_array.include? word[0]
+        "#{word}ay"
+    end
+    if vowel_array.
+        word_array = word.split("")
+        consonant_array = []
+        word_array.each do |i|
+            if vowel_array.include?(word_array[i]) == false
+                letter = word_array.shift
+                consonant_array << letter
+            end
+        end
+    end
+    result = word_array.join + consonant_array.join + "ay"
+    return result
+end
